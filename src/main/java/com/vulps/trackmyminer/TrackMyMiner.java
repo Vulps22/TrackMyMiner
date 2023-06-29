ad234e7e0ae9edcb6ace962aff6d49cd0831e63b
Vulps <ajmcallister5@gmail.com>
2023-06-29 20:09:19 +0100

    Initial Commit

---
+++ src/main/java/com/vulps/trackmyminer/TrackMyMiner.java
@@ -0,0 +1,17 @@
+package com.vulps.trackmyminer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TrackMyMiner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
